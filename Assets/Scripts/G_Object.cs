4d511815d2c989c416a22a3a4173916c1aa56e2c
MeeSh0o <ljhwuwu@qq.com>
2020-05-11 14:44:51 +0800

    功能实现，算法需优化。

---
+++ Assets/Scripts/G_Object.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G_Object : MonoBehaviour
+{
+
+    public Rigidbody Rb;
+    public Gravity Gravity; // 当前重力对象
+    RaycastHit HitInfo;
+    bool isHit = false;
+
+    private void Awake()
+    {
+        Rb = gameObject.GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        RayCast();
+        SetGravity();
+    }
+
+    private void RayCast()
+    {
+        isHit = Physics.Raycast(transform.position, Vector3.down, out HitInfo, 3f, LayerMask.GetMask("Platform"));
+        Vector3 forward = transform.forward;
+        if (HitInfo.collider.gameObject.GetComponent<Gravity>())
+        {
+            Gravity = HitInfo.collider.gameObject.GetComponent<Gravity>();
+        }
+
+        Vector3 newUp;
+        if (isHit)
+        {
+            newUp = HitInfo.normal;
+        }
+        else
+        {
+            newUp = Vector3.up;
+        }
+
+        Vector3 left = Vector3.Cross(forward, newUp);//note: unity use left-hand system, and Vector3.Cross obey left-hand rule.
+        Vector3 newForward = Vector3.Cross(newUp, left);
+        Quaternion oldRotation = transform.rotation;
+        Quaternion newRotation = Quaternion.LookRotation(newForward, newUp);
+
+        float kSoftness = 0.1f;//if do not want softness, change the value to 1.0f
+        Rb.MoveRotation(Quaternion.Lerp(oldRotation, newRotation, kSoftness));
+    }
+
+    private void SetGravity()
+    {
+        Vector3 axis_old = transform.up;
+        Vector3 axis_new = axis_old;
+        if (Gravity)
+        {
+            switch (Gravity.thisType)
+            {
+                case GravityType.Sphere_away:
+                    axis_new = transform.position - Gravity.gameObject.transform.position;
+                    break;
+                case GravityType.Sphere_into:
+                    axis_new = Gravity.gameObject.transform.position - transform.position;
+                    break;
+                case GravityType.Static_away:
+                    axis_new = Gravity.gameObject.transform.up;
+                    break;
+                case GravityType.Static_into:
+                    axis_new = -Gravity.gameObject.transform.up;
+                    break;
+                case GravityType.Unshape_away:
+                    axis_new = HitInfo.normal;
+                    break;
+                case GravityType.UnShape_into:
+                    axis_new = -HitInfo.normal;
+                    break;
+            }
+        }
+        Physics.gravity = axis_new.normalized * Gravity.GravityAccelerate;
+    }
+
+    private bool IsInto(GravityType type) // 似乎没什么用的样子
+    {
+        if (type == GravityType.Static_into || type == GravityType.Sphere_into || type == GravityType.UnShape_into)
+        {
+            return true;
+        }
+        else return false;
+    }
+}
---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public GravityType thisType; // 指定重力类型
+    public float GravityAccelerate = 9.8f;
+}
+
+public enum GravityType
+{
+    Static_into = 0, // 固定方向，指向
+    Static_away = 1, // 固定方向，离向
+    Sphere_into = 2, // 球面，指向球心
+    Sphere_away = 3, // 球面，远离球心
+    UnShape_into = 4, // 不规则面，指向面
+    Unshape_away = 5 // 不规则面，原理面
+}
